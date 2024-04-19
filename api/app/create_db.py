73c9e00ba353cb814a7b714ff53bcf6775366294
Feisal Keli <105209149+feisalkeli@users.noreply.github.com>
2024-04-19 07:17:50 +0300

    configure files

--- api/app/create_db.py
+++
@@ -1,5 +0,0 @@
-from models import db
-
-def create_tables(app, db):
-    with app.app_context():
-        db.create_all()
--- youtube/src/vite-env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ProcessEnv {
-  VITE_RAPIDAPI_KEY: string;
-}
-interface ImportMetaEnv {
-  readonly env: ImportMetaEnv;
-}
